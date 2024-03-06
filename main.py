260b171fefa9cd9f1a1332266d94dce8cf8db01c
Rasukarusan <7gta0you1tube000@gmail.com>
2024-03-06 23:13:15 +0900

    GET /token/{text}を追加

--- main.py
+++ main.py
@@ -4,23 +4,35 @@ import tiktoken
 
 app = FastAPI()
 
+# トークン数取得
+def get_token(text):
+    # @see https://platform.openai.com/tokenizer
+    # @see https://zenn.dev/microsoft/articles/3438cf410cc0b5
+    enc = tiktoken.get_encoding("cl100k_base")
+    tokens = enc.encode(text)
+    # 2024年3月6日現在 @see https://openai.com/pricing
+    inputCost = 0.00001 # 1トークンあたりにかかるドル
+    print(text)
+    print(len(tokens))
+    print(tokens)
+    return {"token": len(tokens), "cost": f'{inputCost * len(tokens):.2f}', "version": "2024-03-06"}
+
+
 class PostTokenRequest(BaseModel):
     text: str
 class PostTokenResponse(BaseModel):
     token: int
     cost: float
     version: str
+
+@app.get("/token/{text}")
+def token(text: str) -> PostTokenResponse:
+    return get_token(text)
+
 @app.post("/token")
-def get_token(req: PostTokenRequest) -> PostTokenResponse:
-    # @see https://platform.openai.com/tokenizer
-    # @see https://zenn.dev/microsoft/articles/3438cf410cc0b5
-    enc = tiktoken.get_encoding("cl100k_base")
-    tokens = enc.encode(req.text)
-    # 2024年3月6日現在 @see https://openai.com/pricing
-    inputCost = 0.00001 # 1トークンあたりにかかるドル
-    print(len(tokens))
-    print(tokens)
-    return {"token": len(tokens), "cost": f'{inputCost * len(tokens):.2f}', "version": "2024-03-06"}
+def token(req: PostTokenRequest) -> PostTokenResponse:
+    return get_token(req.text)
+
 
 
 class EncResponse(BaseModel):
